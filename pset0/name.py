933a023897f577e522365d617b34376448219ac5
Mani bhushan <manibhushan.cs@gmail.com>
2014-11-09 19:18:06 +0530

    problem set 0 of MIT OCW - python - Intro to computer science & programming

---
+++ pset0/name.py
@@ -0,0 +1,8 @@
+# problem set 0 6.00 Intro to comp scie & prog
+# author: mani bhushan
+
+first = raw_input("Enter first name: ")
+
+last = raw_input("Enter last name: ")
+
+print "your full name: ", (first+ " " + last)
