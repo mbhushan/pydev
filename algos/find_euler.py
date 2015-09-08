66a1bfd0c80e4ce8344dc74c0d2f8c89690c5ac9
manibhushan <manibhushan.cs@gmail.com>
2015-09-08 18:16:51 +0530

    added rep for graph

--- algos/find_euler.py
+++ algos/find_euler.py
@@ -10,20 +10,35 @@
 # A possible Eulerian tour would be [1, 2, 3, 1]
 
 
+def create_graph_map(graph):
+    gdict = {}
+    for edge in graph:
+        u = edge[0]
+        v = edge[1]
+        vlist = []
+        if u in gdict:
+            vlist = gdict[u]
+        vlist.append(v)
+        gdict[u] = vlist
+    return gdict
+
+
 def find_eulerian_tour(graph):
     result = []
 
+    gdict = create_graph_map(graph)
+    print "gdict: ", gdict
     return result
 
 
 def test_euler_tour():
     graph1 = [(1, 2), (2, 3), (3, 1)]
-    graph2 = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]
     tour = find_eulerian_tour(graph1)
     print "graph: ", graph1
     print "euler tour: ", tour
-    tour = find_eulerian_tour(graph2)
-    print "graph: ", graph2
+    graph1 = [(1, 2), (2, 3), (3, 1), (3, 4), (4, 3)]
+    tour = find_eulerian_tour(graph1)
+    print "graph: ", graph1
     print "euler tour: ", tour
 
 
