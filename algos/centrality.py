9ca3b4bd192d7e3139aaf21cff777c964c98461f
manibhushan <manibhushan.cs@gmail.com>
2015-09-13 09:17:44 +0530

    finding max distance from a node in a graph

--- algos/centrality.py
+++ algos/centrality.py
@@ -1,15 +1,34 @@
-#
+import Queue
+
 # Write centrality_max to return the maximum distance
 # from a node to all the other nodes it can reach
-#
+
 
 def centrality_max(G, v):
-    # your code here
-    return 0
+    queue = Queue.Queue()
+    marked = {}
+
+    distmap = {}
+    queue.put(v)
+    distmap[v] = 0
+    marked[v] = True
+    while not queue.empty():
+        node = queue.get()
+        dist = distmap[node]
+        for u in G[node]:
+            if u not in marked:
+                distmap[u] = dist + 1
+                marked[u] = True
+                queue.put(u)
+    maxdist = 0
+    for k, v in distmap.items():
+        if v > maxdist:
+            maxdist = v
+    return maxdist
+
 
 #################
 # Testing code
-#
 def make_link(G, node1, node2):
     if node1 not in G:
         G[node1] = {}
@@ -18,6 +37,7 @@ def make_link(G, node1, node2):
         G[node2] = {}
     (G[node2])[node1] = 1
     return G
+
 
 def test():
     chain = ((1,2), (2,3), (3,4), (4,5), (5,6))
@@ -36,3 +56,12 @@ def test():
         make_link(G, n1, n2)
     assert centrality_max(G, 1) == 3
     assert centrality_max(G, 11) == 6
+    print "All centrality tests PASSED!"
+
+
+def main():
+    test()
+
+
+if __name__ == '__main__':
+    main()
