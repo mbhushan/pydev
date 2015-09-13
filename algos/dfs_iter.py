81fe5b6a468b8a50eb765909d8cfea3cd1881398
manibhushan <manibhushan.cs@gmail.com>
2015-09-13 08:41:25 +0530

    iterative implementation of mark components

--- algos/dfs_iter.py
+++ algos/dfs_iter.py
@@ -13,12 +13,13 @@ def mark_component_iter(G, node, marked):
     stack.append(node)
     marked[node] = True
     total_marked = 1
-    while stack:
+    while len(stack) != 0:
+        # print "inside stack!"
         u = stack.pop()
         for neighbor in G[u]:
             if neighbor not in marked:
                 stack.append(neighbor)
-                marked[node] = True
+                marked[neighbor] = True
                 total_marked += 1
     return total_marked
 
@@ -46,11 +47,28 @@ def test():
     assert 4 not in marked
     assert 5 not in marked
     assert 6 not in marked
-    print "All tests PASSED!!"
+    print "All recursive tests PASSED!!"
+
+
+def test_iter():
+    test_edges = [(1, 2), (2, 3), (4, 5), (5, 6)]
+    G = {}
+    for n1, n2 in test_edges:
+        make_link(G, n1, n2)
+    marked = {}
+    assert mark_component_iter(G, 1, marked) == 3
+    assert 1 in marked
+    assert 2 in marked
+    assert 3 in marked
+    assert 4 not in marked
+    assert 5 not in marked
+    assert 6 not in marked
+    print "All iterative tests PASSED!!"
 
 
 def main():
     test()
+    test_iter()
 
 
 if __name__ == '__main__':
