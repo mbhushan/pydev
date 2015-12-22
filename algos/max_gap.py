039f7e7dd32807122d927cd9612f52f93d89ebaa
manibhushan <manibhushan.cs@gmail.com>
2015-12-22 13:40:25 +0530

    max gap in an array

--- algos/max_gap.py
+++ algos/max_gap.py
@@ -1,17 +1,22 @@
 def maximumGap(A):
 	aux = []
 	n = len(A)
+	print "mani"
 
 	for i in range(n):
 		aux.append((A[i], i))
 
-	sorted(aux, key=lambda x:x[0])
+	# sorted(aux, key=lambda x:x[0])
+	aux.sort(key=lambda x: x[0])
+	print "aux", aux
 
 	maxgap = -1
 	index = aux[n-1][1]
 	gap = 0
-
-	for i in range(n-1, 0, -1):
+    
+	for i in range(n-1, -1, -1):
+		print "index", index
+		print "aux_i_1: ", aux[i][1]
 		if aux[i][1] < index:
 			gap = index - aux[i][1]
 		elif aux[i][1] > index:
@@ -25,7 +30,9 @@ def maximumGap(A):
 
 def main():
 	# A = [4, 3, 5, 2, 1, 3, 2, 3]
-	A = [3, 5, 4, 2]
+	# A = [3, 5, 4, 2]
+	A = [1,10]
+	A = [3, 2, 1]
 	res = maximumGap(A)
 	print "res: ", res
 
